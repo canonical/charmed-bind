bf50abf08c6231499ff3d54109c5ee80528dc3d7
Niels Robin-Aubertin <git@niels.fr>
2024-09-18 19:17:38 -0400

    Fix static_root_path when SNAP_DATA env variable is not defined

--- api/dns/settings.py
+++ api/dns/settings.py
@@ -124,7 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(os.getenv("SNAP_DATA"), '/api/static/')
+static_root_path = os.getenv("SNAP_DATA") or BASE_DIR
+STATIC_ROOT = os.path.join(static_root_path, '/api/static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
