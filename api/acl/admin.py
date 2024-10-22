834fcffe250c3268f336e4594487cbf1d9545e6d
Niels Robin-Aubertin <git@niels.fr>
2024-10-22 15:29:37 -0400

    Add ruff configuration and apply recommendations

--- api/acl/admin.py
+++ api/acl/admin.py
@@ -1,4 +1,10 @@
+# Copyright 2024 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Admin module registration."""
+
 from django.contrib import admin
+
 from .models import Acl
 
 admin.site.register(Acl)
